69b461cf95178a9ee89c2fe692f2e774ee9b8084
Yue <yao255@wisc.edu>
2025-10-14 15:12:39 -0500

    Added five_dirs.sh and updated group.csv

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+rm -rf five
+mkdir five
+
+for i in {1..5}
+do
+    mkdir five/dir$i
+    for j in {1..4}
+    do
+        for ((k=1; k<=j; k++))
+        do
+            echo $j >> five/dir$i/file$j
+        done
+    done
+done
